2278a9f66cdd7e401f4d5195126f883e63aeb1ef
GuilhermeHR <guilhermehuertar@gmail.com>
2023-10-20 09:55:45 -0300

    feat: Adicionado código JS para o request do IP

---
+++ app/app.js
@@ -0,0 +1,26 @@
+const apiKey = 'at_A6xi0nYjZOO5AmJ0rRM7OrX0RLVZd'
+const endPoint = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`
+
+const getIpInfo = async endPoint => {
+    try {
+        const response = await fetch(endPoint)
+
+        if(!response.ok) {
+            throw new Error("Couldn't get your IP address")
+        }
+
+        return response.json()
+    } catch(error) {
+        alert(error)
+    }
+}
+
+const showIPInfo = async endPoint => {
+    const { ip, location, isp} = await getIpInfo(endPoint)
+
+    console.log(ip)
+    console.log(location)
+    console.log(isp)
+}
+
+// showIPInfo(endPoint)
